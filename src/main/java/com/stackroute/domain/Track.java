8b94f94990d152235f85289a7bf4689a00e2e379
oohaacp <oohaareddy@gmail.com>
2019-07-25 09:58:21 +0530

    Lombak is finished

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,80 +1,33 @@
 package com.stackroute.domain;
 
 import io.swagger.annotations.ApiModelProperty;
+//import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+// Implementing Lombok with required annotations
+/*
+Internally it generates setters,getters,toString...reduces the hard code for users
+@Data,@NoArgsConstructor,@AllArgsConstructor,@Builder
+ */
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track
 {
     // Declaration of fields in table
     @Id
     int id;
-    @ApiModelProperty(notes = "Generated Track-firstName")
     String firstName;
-    @ApiModelProperty(notes = "Generated Track-lastName")
     String lastName;
-    @ApiModelProperty(notes = "Generated Track-age")
     int age;
 
-    public Track()
-    {
-    }
-
-    public Track(int id, String firstName, String lastName, int age) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    public int getId()
-    {
-        return id;
-    }
-
-    public void setId(int id)
-    {
-        this.id = id;
-    }
-
-    public String getFirstName()
-    {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName)
-    {
-        this.firstName = firstName;
-    }
-
-    public String getLastName(String lastName)
-    {
-        return this.lastName;
-    }
-
-    public void setLastName(String lastName)
-    {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 
 }
 
