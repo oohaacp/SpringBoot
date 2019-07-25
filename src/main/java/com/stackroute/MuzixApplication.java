d4f178dac9e476321aad314d701bfbbf8d43b6e5
oohaacp <oohaareddy@gmail.com>
2019-07-25 12:32:29 +0530

    hard codeded is finished

--- src/main/java/com/stackroute/MuzixApplication.java
+++ src/main/java/com/stackroute/MuzixApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 //@Configuration
 //@EnableAutoConfiguration
 //@ComponentScan
 @SpringBootApplication
+@PropertySource("application.properties")
 public class MuzixApplication
 {
 
--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -2,10 +2,7 @@ package com.stackroute.domain;
 
 import io.swagger.annotations.ApiModelProperty;
 //import jdk.jfr.DataAmount;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -16,19 +13,78 @@ Internally it generates setters,getters,toString...reduces the hard code for use
  */
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Track
 {
     // Declaration of fields in table
     @Id
+    @Value("${app.id}")
     int id;
+    @Value("${app.firstName}")
     String firstName;
+    @Value("${app.lastName}")
     String lastName;
+    @Value("${app.age}")
     int age;
 
 
-}
 
+    public Track()
+    {
+    }
+
+    public Track(int id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
 
