c67bb15721df4398cb2bc9ad87bc55591f5bb289
oohaacp <51943028+oohaacp@users.noreply.github.com>
2019-08-07 10:46:39 +0530

    Update TrackService.java

--- src/main/java/com/stackroute/service/TrackService.java
+++ src/main/java/com/stackroute/service/TrackService.java
@@ -9,7 +9,7 @@ public interface TrackService
     // Methods to implement CRUD.
     public Track saveTrack(Track track) throws Exception;
     public List<Track> getAllTracks();
-    public void deleteTrack(int id);
+    public Track deleteTrack(int id);
     public Track updateTrack(Track track) throws Exception;
     public List<Track> trackByName(String name);
     public void getUrlData() throws Exception;
