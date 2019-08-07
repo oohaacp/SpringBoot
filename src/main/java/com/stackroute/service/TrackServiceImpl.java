831a1f0437fa90fed3e514396b54d959d245aab2
oohaacp <51943028+oohaacp@users.noreply.github.com>
2019-08-07 11:09:07 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.optional;
+
 @Service
 public class TrackServiceImpl implements TrackService {
     TrackRepository trackRepository;
@@ -29,7 +31,7 @@ public class TrackServiceImpl implements TrackService {
 
         if(saveUser==null)
         {
-            throw new TrackAlreadyExistsException("Track already present");
+            throw new TrackAlreadyExistsException("Track is not exist");
         }
         return saveUser;
 
@@ -61,12 +63,16 @@ public class TrackServiceImpl implements TrackService {
 
     }
 
-
-
     @Override
-    public void deleteTrack(int id) {
-        trackRepository.deleteById(id);
+    public Track deleteTrack(int id) {
+        Optional<Track> track =null;
+        if(trackRepository.existsById(id) == true) {
+            trackRepository.deleteById(id);
+            track= trackRepository.findById(id);
+        }
+        return track.get();
     }
+    
     @Override
     public Track trackByName(String firstName)  {
         return trackRepository.trackByName(firstName);
