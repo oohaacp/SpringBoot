de0b255fcf4f60af3448aa92da80ea5d1c3df587
oohaacp <51943028+oohaacp@users.noreply.github.com>
2019-08-07 11:10:31 +0530

    Update TrackService.java

--- src/main/java/com/stackroute/service/TrackService.java
+++ src/main/java/com/stackroute/service/TrackService.java
@@ -10,7 +10,7 @@ public interface TrackService
 {
     public Track saveUser(Track track) throws TrackAlreadyExistsException;
     public List<Track>getAllUsers();
-    public void deleteTrack(int id);
+    public Track deleteTrack(int id);
     public Track updateTrack(Track track) throws TrackNotFoundException;
     public Track trackByName(String firstName);
 }
